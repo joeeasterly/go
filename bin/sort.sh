e5ef52527c10eddbfcbd18fcb664deb973c7c133
Joe Easterly <joeeasterly@gmail.com>
2023-09-01 15:20:58 -0400

    sorted

---
+++ bin/sort.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for file in *.json; do
+    prefix=${file:0:1}
+    subfolder=${file:1:1}
+
+    mkdir -p "$prefix/$subfolder"
+    mv "$file" "$prefix/$subfolder/$file"
+done
+
