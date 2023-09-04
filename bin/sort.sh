2f1a90afd87ccee47a5de00677b5fc3ddc44d2ba
Joe Easterly <joeeasterly@gmail.com>
2023-09-03 20:21:54 -0400

    updated permalinks

--- bin/sort.sh
+++ bin/sort.sh
@@ -4,7 +4,7 @@ for file in *.json; do
     prefix=${file:0:1}
     subfolder=${file:1:1}
 
-    mkdir -p "$prefix/$subfolder"
-    mv "$file" "$prefix/$subfolder/$file"
+    mkdir -p "../$prefix/$subfolder"
+    mv "$file" "../$prefix/$subfolder/$file"
 done
 
