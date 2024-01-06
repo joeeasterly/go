11ac7ccd1144bf16d5506b04c8e5f39598ed109c
Joe Easterly <joeeasterly@gmail.com>
2024-01-06 17:27:36 -0500

    updated scripts and metadata

--- bin/library/parse_upc.py
+++ bin/library/parse_upc.py
@@ -1,11 +1,13 @@
 def parse_upc(existing_upc = None):
     permitted_characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     upc_prompt = "Enter UPC: "
+    upc = ""
     if existing_upc:
         upc_prompt = f"Enter UPC [{existing_upc}]: "
     upc = input(upc_prompt)
+    if upc:
+        # Remove all characters in the label except those in the permitted_characters list.
+        upc = "".join([character for character in upc if character in permitted_characters])
     if not upc:
         upc = existing_upc
-    # Remove all characters in the label except those in the permitted_characters list.
-    upc = "".join([character for character in upc if character in permitted_characters])
     return upc
