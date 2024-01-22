7444d1af687789cd2556993bbaed8b3627be0526
Joe Easterly <joeeasterly@gmail.com>
2024-01-21 19:46:00 -0500

    added shelving records

--- bin/export_shelves.py
+++ bin/export_shelves.py
@@ -1,21 +1,46 @@
 #!/usr/bin/env python3
+import os
+import json
+from bson import json_util  # Import json_util for ObjectId serialization
 from library.connect_mungo import connect_mungo
 from library.input_shcn import input_shcn
 from library.get_storage_record import get_storage_record
-collection = connect_mungo()
-# Search mungo for records where input_shcn.shcn matches the beginning of the shcn field in the collection.
 
+collection = connect_mungo()
+
+# Specify the directory to save the JSON files
+output_directory = "/home/joeeasterly/Documents/GitHub/go/shelves"
+
+# Search mungo for records where input_shcn.shcn matches the beginning of the shcn field in the collection.
 shelf_list = collection.distinct("shcn")
+
+# Counter for the number of files written
+file_counter = 0
+
+# Total number of files
+total_files = len(shelf_list)
+
 for shelving_record in shelf_list:
     metadata = get_storage_record(shelving_record)
     if metadata:
-        label = metadata["label"]
         shcn = metadata["shcn"]
-        print(shcn, label)
-        shelf_contents = collection.find({"shcn": shcn})
-        for item in shelf_contents:
-            print(" --", item["identifier"], item["label"])
+        filename = os.path.join(output_directory, f"{shcn.lower()}.json")
 
-# shcn, shelf, bay, container, slot, analysis = input_shcn()
-# for record in collection.find({"shcn": {"$regex": "^" + shcn}}):
-#     print(record["identifier"], record["label"])
+        # Create a dictionary for the shelving record
+        record_dict = {"shelving_record": json.loads(json.dumps(metadata, default=json_util.default)), "shelf_contents": []}
+
+        shelf_contents = collection.find({"shcn": shcn})
+
+        # Add each item in shelf_contents to the dictionary
+        for item in shelf_contents:
+            record_dict["shelf_contents"].append(json.loads(json.dumps(item, default=json_util.default)))
+
+        # Write the dictionary to a JSON file
+        with open(filename, "w") as json_file:
+            json.dump(record_dict, json_file, indent=2, default=json_util.default)
+
+        # Increment the file counter and print the filename to stdout
+        file_counter += 1
+        print(f"File {file_counter}/{total_files}: {os.path.basename(filename)}")
+
+# The resulting JSON files will be saved in the specified directory (/home/joeeasterly/Documents/GitHub/go/shelves).
